b4d91aa366783f0bf1291fecc6277889b604a71a
jszum <196018@student.pwr.edu.pl>
2014-11-20 13:59:10 +0100

    Neccessary comments in file.

--- lab4/thread.c
+++ lab4/thread.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <unistd.h>
 
+/****Define const values****/
 #define ADD_COUNT 20
 #define IN_AMOUNT 100
 
@@ -14,19 +15,21 @@
 #define TAKE 1
 #define RELEASE 0
 
+#define timer 1
+
+/****Init mutex mechanisms****/
 pthread_mutex_t acc1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t acc2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cash =  PTHREAD_COND_INITIALIZER;
 pthread_cond_t acc1Taken =  PTHREAD_COND_INITIALIZER;
 pthread_cond_t acc2Taken =  PTHREAD_COND_INITIALIZER;
 
+/****Global value for representing accounts and conditional variables****/
 int account1 = 0;
 int busy1 = 0;
 int account2 = 0;
 int busy2 = 0;
 
-int timer = 1;
-int ival = 1;
 
 void printAcc1()
 {
@@ -38,22 +41,33 @@ void printAcc2()
 	printf("status of acc2 is %d\n", account2);
 }
 
+/****Thread that makes some income for account 1****/
 void* add(void * arg)
 {
 	for(int i = 0; i < ADD_COUNT; i++)
 	{
+		/****lock access to acc1****/
 		pthread_mutex_lock(&acc1);
-			if(busy1) pthread_cond_wait(&acc1Taken, &acc1);
+			
+			/****if busy then wait on cond_var****/
+			while(busy1) pthread_cond_wait(&acc1Taken, &acc1);
 			busy1 = TAKE;
 		pthread_mutex_unlock(&acc1);
 
+		/****Critical section - do your job****/
 		int local_acc = account1;
 		local_acc = local_acc + IN_AMOUNT;
 		account1 = local_acc;
 
 		printf("Adder :status of acc1 is %d\n", account1);
-		if(timer) usleep(ival);
+		
+		if(timer) 
+		{
+			int ival = rand()%20000;
+			usleep(ival);
+		}
 
+		/****Inform others that finished****/
 		pthread_mutex_lock(&acc1);
 			busy1 = 0;
 			pthread_cond_signal(&acc1Taken);
@@ -63,15 +77,19 @@ void* add(void * arg)
 	return 0;
 }
 
+/****Thread to withdraw some money****/
 void* withdraw(void * arg)
 {
 	for(int i = 0; i < OUT_COUNT; i++)
 	{
 
+		/****Try to lock access to acc1****/
 		pthread_mutex_lock(&acc1);
+			
+			/****wait if busy****/
+			while(busy1) pthread_cond_wait(&acc1Taken, &acc1);
 
-			if(busy1) pthread_cond_wait(&acc1Taken, &acc1);
-
+			/****wait if not enough money****/
 			while(account1 < OUT_AMOUNT)	
 			{
 				pthread_cond_wait(&cash, &acc1);
@@ -80,14 +98,19 @@ void* withdraw(void * arg)
 			busy1 = TAKE;
 		pthread_mutex_unlock(&acc1);
 
-
+		/****Critical section - do your job****/
 		int local_acc = account1;
 		local_acc = local_acc - OUT_AMOUNT;
 		account1 = local_acc;
 
 		printf("--Withdrawer: status of acc1 is %d\n", account1);
-		if(timer) usleep(ival);
+		if(timer) 
+		{
+			int ival = rand()%20000;
+			usleep(ival);
+		}
 
+		/****Informt others that finished ****/
 		pthread_mutex_lock(&acc1);
 			busy1 = RELEASE;
 			pthread_cond_signal(&acc1Taken);
@@ -97,19 +120,21 @@ void* withdraw(void * arg)
 	return 0;
 }
 
+/****Thread that moves some amount of money from one account to another ****/
 void* move(void * arg)
 {
 
+	/****All locking part for first account - try to access, check if enough money to withdraw etc.****/
 	pthread_mutex_lock(&acc1);
 
-	if(busy1) pthread_cond_wait(&acc1Taken, &acc1);
+		while(busy1) pthread_cond_wait(&acc1Taken, &acc1);
 
-	while(account1 < MOVE_AMOUNT)	
-	{
-		pthread_cond_wait(&cash, &acc1);
-	}
+		while(account1 < MOVE_AMOUNT)	
+		{
+			pthread_cond_wait(&cash, &acc1);
+		}
 
-	busy1 = TAKE;
+		busy1 = TAKE;
 	pthread_mutex_unlock(&acc1);
 
 	int local_acc = account1;
@@ -117,7 +142,12 @@ void* move(void * arg)
 	account1 = local_acc;
 
 	printf("\tMover: status of acc1 is %d\n", account1);
-	if(timer) usleep(ival);
+	if(timer) 
+	{
+		int ival = rand()%20000;
+		usleep(ival);
+	}
+		
 
 	pthread_mutex_lock(&acc1);
 		busy1 = RELEASE;
@@ -125,9 +155,9 @@ void* move(void * arg)
 	pthread_mutex_unlock(&acc1);
 
 
-
+	/****All locking part for second account - try to access, add some money****/
 	pthread_mutex_lock(&acc2);
-		if(busy2) pthread_cond_wait(&acc2Taken, &acc2);
+		while(busy2) pthread_cond_wait(&acc2Taken, &acc2);
 		busy2 = TAKE;
 	pthread_mutex_unlock(&acc2);
 
@@ -136,7 +166,11 @@ void* move(void * arg)
 	account2 = local_acc;
 
 	printf("\tMover :status of acc2 is %d\n", account2);
-	if(timer) usleep(ival);
+	if(timer) 
+	{
+		int ival = rand()%20000;
+		usleep(ival);
+	}
 
 	pthread_mutex_lock(&acc2);
     		busy2 = RELEASE;
@@ -149,17 +183,22 @@ void* move(void * arg)
 
 int main(int argc, char** argv)
 {
+	srand(time(NULL));	
+
 	void* status;
 	pthread_t tids[] = { 0, 1, 2 };
 
-	pthread_create(&tids[0], NULL, add, NULL);
-	pthread_create(&tids[1], NULL, withdraw, NULL);
-	pthread_create(&tids[2], NULL, move, NULL);
+	/****Create threads****/
+	pthread_create(&tids[0], NULL, move, NULL);
+	pthread_create(&tids[1], NULL, add, NULL);
+	pthread_create(&tids[2], NULL, withdraw, NULL);
 
+	/****Wait for threads to end****/
 	pthread_join(tids[0], &status);
 	pthread_join(tids[1], &status);
 	pthread_join(tids[2], &status);
 	
+	/****At the end - print the status to check if everything is right****/
 	printAcc1();
 	printAcc2();
 	
